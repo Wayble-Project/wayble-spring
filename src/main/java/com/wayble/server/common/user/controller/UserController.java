f849e88bd1c2b40a2d658429daac02e782b64275
KiSeungMin <ksm011212@gmail.com>
2025-07-04 23:44:16 +0900

    [feat] 유저 관련 예외 구조체 정의

--- src/main/java/com/wayble/server/common/user/controller/UserController.java
+++ src/main/java/com/wayble/server/common/user/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.wayble.server.common.user.controller;
 
+import com.wayble.server.common.exception.ApplicationException;
 import com.wayble.server.common.response.CommonResponse;
+import com.wayble.server.common.user.exception.UserErrorCase;
 import com.wayble.server.common.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +16,15 @@ public class UserController {
 
     private final UserService userService;
 
+    // 참고용 컨트롤러(지우셔도 돼요)
+    @GetMapping("/hello")
     public CommonResponse<String> hello() {
         return CommonResponse.success("hello");
+    }
+
+    // 예외 사용 참고용 컨트롤러(지우셔도 돼요)
+    @GetMapping("/ex")
+    public CommonResponse<String> exception() {
+        throw new ApplicationException(UserErrorCase.USER_NOT_FOUND);
     }
 }
