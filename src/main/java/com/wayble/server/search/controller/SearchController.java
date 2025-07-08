ef3d8bdfcc4c36644c3e90c1f8149a7312ffbb79
KiSeungMin <ksm011212@gmail.com>
2025-07-08 12:29:12 +0900

    [feat] controller에서 condition dto를 @ModelAttribute로 받도록 수정

--- src/main/java/com/wayble/server/search/controller/SearchController.java
+++ src/main/java/com/wayble/server/search/controller/SearchController.java
@@ -1,13 +1,15 @@
 package com.wayble.server.search.controller;
 
 import com.wayble.server.common.response.CommonResponse;
+import com.wayble.server.search.dto.WaybleZoneDocumentRegisterDto;
+import com.wayble.server.search.dto.WaybleZoneSearchConditionDto;
+import com.wayble.server.search.dto.WaybleZoneSearchResponseDto;
 import com.wayble.server.search.entity.WaybleZoneDocument;
 import com.wayble.server.search.service.SearchService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,9 +23,14 @@ public class SearchController {
         return CommonResponse.success(searchService.getWaybleZoneDocumentById(1L));
     }
 
-    @PostMapping("/test")
-    public CommonResponse<String> registerEntity() {
-        searchService.save();
-        return CommonResponse.success("테스트 객체 등록 완료!");
+    @GetMapping("")
+    public CommonResponse<List<WaybleZoneSearchResponseDto>> findByCondition(@ModelAttribute WaybleZoneSearchConditionDto conditionDto) {
+        return CommonResponse.success(searchService.searchWaybleZonesByCondition(conditionDto));
+    }
+
+    @PostMapping("")
+    public CommonResponse<String> registerDocumentFromDto(@RequestBody WaybleZoneDocumentRegisterDto registerDto) {
+        searchService.saveDocumentFromDto(registerDto);
+        return CommonResponse.success("Wayble Zone Document 등록 완료!");
     }
 }
