91eee21b8971932493e9c6b17704b0a682aac64a
KiSeungMin <ksm011212@gmail.com>
2025-07-08 18:32:51 +0900

    [feat] 특정 단어가 포함된 웨이블존 거리순 조회 테스트 메서드 구현 완료

--- src/main/java/com/wayble/server/search/dto/WaybleZoneSearchConditionDto.java
+++ src/main/java/com/wayble/server/search/dto/WaybleZoneSearchConditionDto.java
@@ -11,7 +11,7 @@ public record WaybleZoneSearchConditionDto(
         @NonNull
         Double longitude,
         Double radiusKm,
-        String name,
+        String zoneName,
         WaybleZoneType zoneType
 ) {
 }
