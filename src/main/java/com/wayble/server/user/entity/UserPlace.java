86a76392ef44c9ffcda5c6976c816409d215ffac
seungin <tmddls91@inha.edu>
2025-07-10 02:21:19 +0900

    [feat] 내가(유저가) 저장한 장소 엔티티 구현

---
+++ src/main/java/com/wayble/server/user/entity/UserPlace.java
@@ -0,0 +1,29 @@
+package com.wayble.server.user.entity;
+
+import com.wayble.server.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user_place") // 유져가 저장한 장소
+public class UserPlace extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
