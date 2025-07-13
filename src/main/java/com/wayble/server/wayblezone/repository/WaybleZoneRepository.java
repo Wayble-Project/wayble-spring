4441075bcbd3a7e964b67856348fbf76891a6a06
seungin <tmddls91@inha.edu>
2025-07-13 13:05:00 +0900

    [feat] 웨이블존 목록 조회를 위한 레포지토리 구현

--- src/main/java/com/wayble/server/wayblezone/repository/WaybleZoneRepository.java
+++ src/main/java/com/wayble/server/wayblezone/repository/WaybleZoneRepository.java
@@ -3,5 +3,8 @@ package com.wayble.server.wayblezone.repository;
 import com.wayble.server.wayblezone.entity.WaybleZone;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WaybleZoneRepository extends JpaRepository<WaybleZone, Long> {
+    List<WaybleZone> findByAddress_CityContainingAndZoneType(String city, com.wayble.server.wayblezone.entity.WaybleZoneType category);
 }
