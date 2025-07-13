dc37c306347513d1aecb8e7f7dce61bd478bcbe7
seungin <tmddls91@inha.edu>
2025-07-13 21:24:25 +0900

    [refactor] UserPlaceRequestDto로 DTO 파일 이름 변경

--- src/main/java/com/wayble/server/user/dto/UserPlaceRequestDto.java
+++ src/main/java/com/wayble/server/user/dto/UserPlaceRequestDto.java
@@ -2,7 +2,7 @@ package com.wayble.server.user.dto;
 
 import jakarta.validation.constraints.NotNull;
 
-public record UserPlaceSaveRequestDto(
+public record UserPlaceRequestDto(
         @NotNull Long userId,
         @NotNull Long waybleZoneId,
         @NotNull String title
