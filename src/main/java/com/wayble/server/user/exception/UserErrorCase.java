03f4d082f7ded520f05b7c4a6f8314412775d76c
seungin <tmddls91@inha.edu>
2025-07-13 21:19:16 +0900

    [feat] 유저 에러 로직에 에러 코드 추가

--- src/main/java/com/wayble/server/user/exception/UserErrorCase.java
+++ src/main/java/com/wayble/server/user/exception/UserErrorCase.java
@@ -8,7 +8,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum UserErrorCase implements ErrorCase {
 
-    USER_NOT_FOUND(400, 1001, "사용자를 찾을 수 없습니다.");
+    USER_NOT_FOUND(400, 1001, "사용자를 찾을 수 없습니다."),
+    WAYBLE_ZONE_NOT_FOUND(404, 1002, "해당 웨이블존을 찾을 수 없습니다."),
+    PLACE_ALREADY_SAVED(400, 1003, "이미 저장한 장소입니다.");
 
     private final Integer httpStatusCode;
     private final Integer errorCode;
