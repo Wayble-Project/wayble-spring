7b39ac83a78f0cce36f30cab009d450ba4dc42f7
KiSeungMin <ksm011212@gmail.com>
2025-07-14 19:02:41 +0900

    [fix] reactive elastic search  관련 문제 해결

--- src/main/java/com/wayble/server/ServerApplication.java
+++ src/main/java/com/wayble/server/ServerApplication.java
@@ -2,10 +2,15 @@ package com.wayble.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ReactiveElasticsearchRepositoriesAutoConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = ReactiveElasticsearchRepositoriesAutoConfiguration.class
+)
 @EnableJpaAuditing
+@EnableElasticsearchRepositories(basePackages = {"com.wayble.server.search.repository", "com.wayble.server.recommend.repository"})
 public class ServerApplication {
 
 	public static void main(String[] args) {
