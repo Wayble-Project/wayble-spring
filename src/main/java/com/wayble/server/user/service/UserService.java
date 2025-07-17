c474bc0b960d0e02723c6737f64b4364180bc759
seungin <tmddls91@inha.edu>
2025-07-17 16:32:28 +0900

    [feat] 회원가입 로직을 위한 서비스 구현 및 관련 에러 케이스 추가

--- src/main/java/com/wayble/server/user/service/UserService.java
+++ src/main/java/com/wayble/server/user/service/UserService.java
@@ -1,9 +1,12 @@
 package com.wayble.server.user.service;
 
 import com.wayble.server.common.exception.ApplicationException;
+import com.wayble.server.user.dto.UserRegisterRequestDto;
+import com.wayble.server.user.entity.User;
 import com.wayble.server.user.exception.UserErrorCase;
 import com.wayble.server.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +14,25 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    // 회원가입
+    public void signup(UserRegisterRequestDto req) {
+        if (userRepository.existsByEmail(req.email())) {
+            throw new ApplicationException(UserErrorCase.USER_ALREADY_EXISTS);
+        }
+        User user = User.createUser(
+                req.nickname(),
+                req.username(),
+                req.email(),
+                passwordEncoder.encode(req.password()),
+                req.birthDate(),
+                req.gender(),
+                req.loginType(),
+                req.userType()
+        );
+        userRepository.save(user);
+    }
 
     public void makeException() {
         throw new ApplicationException(UserErrorCase.USER_NOT_FOUND);
