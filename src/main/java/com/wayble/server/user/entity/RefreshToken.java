2e7f2257743f07d2250318778cbf482df6a31891
seungin <tmddls91@inha.edu>
2025-07-18 19:57:53 +0900

    [feat] refresh token 관련 엔티티 생성

---
+++ src/main/java/com/wayble/server/user/entity/RefreshToken.java
@@ -0,0 +1,26 @@
+package com.wayble.server.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "refresh_token")
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true) // 1인 1토큰
+    private Long userId;
+
+    @Column(nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    private Long expiry; // 만료 시간
+}
