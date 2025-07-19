469de36a79aae46a48a665cdc20670ad115c5553
KiSeungMin <ksm011212@gmail.com>
2025-07-19 15:53:40 +0900

    [feat] soft delete된 필드를 되살리는 로직 추가

--- src/main/java/com/wayble/server/user/repository/UserRepository.java
+++ src/main/java/com/wayble/server/user/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.wayble.server.user.repository;
 import com.wayble.server.user.entity.LoginType;
 import com.wayble.server.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -10,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmailAndLoginType(String email, LoginType loginType);
     Optional<User> findByEmailAndLoginType(String email, LoginType loginType);
+    @Query(value = "SELECT * FROM user WHERE email = :email AND login_type = :#{#loginType.name()} AND deleted_at IS NOT NULL", nativeQuery = true)
+    Optional<User> findDeletedUserByEmailAndLoginType(String email, @Param("loginType") LoginType loginType);
 }
