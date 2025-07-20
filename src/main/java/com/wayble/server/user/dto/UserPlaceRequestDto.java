6a08c023c633b5dd48b4f4df1740e21673340c81
seungin <tmddls91@inha.edu>
2025-07-20 19:16:42 +0900

    [refactor] requestBody에 userId 제거

--- src/main/java/com/wayble/server/user/dto/UserPlaceRequestDto.java
+++ src/main/java/com/wayble/server/user/dto/UserPlaceRequestDto.java
@@ -3,7 +3,6 @@ package com.wayble.server.user.dto;
 import jakarta.validation.constraints.NotNull;
 
 public record UserPlaceRequestDto(
-        @NotNull Long userId,
         @NotNull Long waybleZoneId,
         @NotNull String title
 ) {}
