16c4f2c021f39abe27fa2087f2dd944dc8859665
hyoin <hyoinyang02@gmail.com>
2025-07-21 20:33:08 +0900

    [fix] dto 수정

--- src/main/java/com/wayble/server/direction/dto/TransportationResponseDto.java
+++ src/main/java/com/wayble/server/direction/dto/TransportationResponseDto.java
@@ -1,5 +1,6 @@
 package com.wayble.server.direction.dto;
 
+import com.wayble.server.direction.entity.DirectionType;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.ZonedDateTime;
@@ -7,35 +8,14 @@ import java.util.List;
 
 @Schema(description = "대중교통 길찾기 응답 DTO")
 public record TransportationResponseDto(
-        List<Route> routes,
+        List<Step> routes,
         PageInfo pageInfo
 ) {
-    public record Route(
-            int totalTime,
-            ZonedDateTime arriveTime,
-            List<Step> steps
-    ) {}
-
     public record Step(
-            String mode, // 예: START, WALK, SUBWAY, BUS, FINISH
-            String address, // mode에 따라 null일 수 있음
+            DirectionType mode, // 예: START, WALK, SUBWAY, BUS, FINISH
+            String routeName, // mode에 따라 null일 수 있음
             String from,
-            String to,
-            String line,
-            List<Path> path, // WALK일 때 사용
-            SubwayInformation information, // SUBWAY일 때 사용
-            String steps // BUS일 때 사용
-    ) {}
-
-    public record Path(
-            String mode, // WALK 등
-            int time // 분 단위
-    ) {}
-
-    public record SubwayInformation(
-            List<String> wheelchair,
-            List<String> elevator,
-            boolean accessibleRestroom
+            String to
     ) {}
 
     public record PageInfo(
