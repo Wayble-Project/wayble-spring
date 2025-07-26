de40911a405866e0daf9caf57b0183bd9cfc9998
seungin <tmddls91@inha.edu>
2025-07-26 17:51:41 +0900

    [fix] 회원가입 로직 변경에 따른 로그인 요청 필드 변경

--- src/main/java/com/wayble/server/user/dto/UserLoginRequestDto.java
+++ src/main/java/com/wayble/server/user/dto/UserLoginRequestDto.java
@@ -6,9 +6,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record UserLoginRequestDto(
-        @NotBlank(message = "이름 또는 닉네임은 필수입니다")
-        String name,
-
         @NotBlank(message = "이메일은 필수입니다")
         @Email(message = "유효한 이메일 형식이 아닙니다")
         String email,
