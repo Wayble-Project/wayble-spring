bd23fda6f5f5b4a21989f684f505d6c48d9beff7
seungin <tmddls91@inha.edu>
2025-07-26 17:16:05 +0900

    [feat] 유저 정보 등록 관련 Dto 구현

---
+++ src/main/java/com/wayble/server/user/dto/UserInfoRegisterRequestDto.java
@@ -0,0 +1,26 @@
+package com.wayble.server.user.dto;
+
+
+import com.wayble.server.user.entity.Gender;
+import com.wayble.server.user.entity.UserType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class UserInfoRegisterRequestDto {
+    @NotBlank(message = "닉네임은 필수입니다.")
+    private String nickname;
+
+    @NotBlank(message = "생년월일은 필수입니다.")
+    private String birthDate; // YYYY-MM-DD
+
+    @NotNull(message = "성별은 필수입니다.")
+    private Gender gender;
+
+    @NotNull(message = "유저 타입은 필수입니다.")
+    private UserType userType;
+
+    private String disabilityType; // 장애 유형, (userType == DISABLED만 값 존재)
+    private String mobilityAid; // 이동보조수단, (userType == DISABLED만 값 존재)
+}
