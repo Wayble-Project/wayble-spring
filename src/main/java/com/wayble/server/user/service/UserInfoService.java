2226abecfb63f077a887a2ec833b47219027836d
seungin <tmddls91@inha.edu>
2025-07-28 01:41:09 +0900

    [feat] 유저 정보 수정 관련 서비스 로직 추가 (UserInfoService)

--- src/main/java/com/wayble/server/user/service/UserInfoService.java
+++ src/main/java/com/wayble/server/user/service/UserInfoService.java
@@ -3,6 +3,7 @@ package com.wayble.server.user.service;
 
 import com.wayble.server.common.exception.ApplicationException;
 import com.wayble.server.user.dto.UserInfoRegisterRequestDto;
+import com.wayble.server.user.dto.UserInfoUpdateRequestDto;
 import com.wayble.server.user.entity.User;
 import com.wayble.server.user.entity.UserType;
 import com.wayble.server.user.exception.UserErrorCase;
@@ -45,6 +46,40 @@ public class UserInfoService {
         } else {
             user.setDisabilityType(null);
             user.setMobilityAid(null);
+        }
+
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public void updateUserInfo(Long userId, UserInfoUpdateRequestDto dto) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ApplicationException(UserErrorCase.USER_NOT_FOUND));
+
+        if (dto.getNickname() != null) {
+            user.setNickname(dto.getNickname());
+        }
+
+        // 생년월일 수정
+        if (dto.getBirthDate() != null) {
+            user.setBirthDate(LocalDate.parse(dto.getBirthDate()));
+        }
+
+        // 성별 수정
+        if (dto.getGender() != null) {
+            user.setGender(dto.getGender());
+        }
+
+        // 유저 타입 수정
+        if (dto.getUserType() != null) {
+            user.setUserType(dto.getUserType());
+            if (dto.getUserType() == UserType.DISABLED) {
+                user.setDisabilityType(dto.getDisabilityType());
+                user.setMobilityAid(dto.getMobilityAid());
+            } else {
+                user.setDisabilityType(null);
+                user.setMobilityAid(null);
+            }
         }
 
         userRepository.save(user);
