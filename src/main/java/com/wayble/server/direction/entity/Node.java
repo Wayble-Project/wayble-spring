1605ecd53c647b31a2fe256e067431a3f99713aa
hyoin <hyoinyang02@gmail.com>
2025-07-30 12:37:46 +0900

    [fix] 노드를 비교하는 조건 변경

--- src/main/java/com/wayble/server/direction/entity/Node.java
+++ src/main/java/com/wayble/server/direction/entity/Node.java
@@ -54,7 +54,7 @@ public class Node {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Node)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
         Node node = (Node) o;
         return id != null && id.equals(node.id);
     }
