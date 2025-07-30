2dd4bc3d400412fa4e224b1072ea019ffcc6467c
hyoin <hyoinyang02@gmail.com>
2025-07-30 12:37:34 +0900

    [fix] 레포지토리의 id 변경

--- src/main/java/com/wayble/server/direction/repository/EdgeRepository.java
+++ src/main/java/com/wayble/server/direction/repository/EdgeRepository.java
@@ -3,5 +3,5 @@ package com.wayble.server.direction.repository;
 import com.wayble.server.direction.entity.Edge;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EdgeRepository extends JpaRepository<Edge, Integer> {
+public interface EdgeRepository extends JpaRepository<Edge, Long> {
 }
--- src/main/java/com/wayble/server/direction/repository/NodeRepository.java
+++ src/main/java/com/wayble/server/direction/repository/NodeRepository.java
@@ -3,5 +3,5 @@ package com.wayble.server.direction.repository;
 import com.wayble.server.direction.entity.Node;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NodeRepository extends JpaRepository<Node, Integer> {
+public interface NodeRepository extends JpaRepository<Node, Long> {
 }
