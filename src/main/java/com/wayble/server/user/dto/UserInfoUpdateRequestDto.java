111d37d99e3c78834540389789b4447f42eb5a2e
seungin <tmddls91@inha.edu>
2025-07-30 16:54:01 +0900

    [refactor] 유저 정보 수정 관련 Dto 필드 List로 변경 (장애유형,이동보조수단)

--- src/main/java/com/wayble/server/user/dto/UserInfoUpdateRequestDto.java
+++ src/main/java/com/wayble/server/user/dto/UserInfoUpdateRequestDto.java
@@ -4,12 +4,14 @@ import com.wayble.server.user.entity.Gender;
 import com.wayble.server.user.entity.UserType;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class UserInfoUpdateRequestDto {
     private String nickname;         // 변경할 닉네임 (nullable)
     private String birthDate;        // YYYY-MM-DD (nullable)
     private Gender gender;           // MALE, FEMALE, UNKNOWN (nullable)
     private UserType userType;       // GENERAL, DISABLED, COMPANION (nullable)
-    private String disabilityType;   // userType이 DISABLED일 때만 값, 아니면 null
-    private String mobilityAid;      // userType이 DISABLED일 때만 값, 아니면 null
+    private List<String> disabilityType;  // userType이 DISABLED일 때만 값, 아니면 null
+    private List<String> mobilityAid;      // userType이 DISABLED일 때만 값, 아니면 null
 }
