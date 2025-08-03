8fadc51d366931e52f8eb23850936ad44fbe46a8
zyovn <yui2507@naver.com>
2025-08-03 16:02:40 +0900

    [fix] place name nullable = true로 수정

--- src/main/java/com/wayble/server/direction/entity/Place.java
+++ src/main/java/com/wayble/server/direction/entity/Place.java
@@ -16,7 +16,7 @@ public class Place extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name")
     private String name;
 
     @Column(name = "address", nullable = false)
