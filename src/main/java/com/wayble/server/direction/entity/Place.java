dbac114b4265898624b472d5d8d35eb55ecc222a
zyovn <yui2507@naver.com>
2025-08-03 15:39:05 +0900

    [feat] place 엔티티 설계

---
+++ src/main/java/com/wayble/server/direction/entity/Place.java
@@ -0,0 +1,47 @@
+package com.wayble.server.direction.entity;
+
+import com.wayble.server.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Place extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "latitude", nullable = false)
+    private long latitude;
+
+    @Column(name = "longitude", nullable = false)
+    private long longitude;
+
+    @Builder
+    public Place(String name, String address, long latitude, long longitude) {
+        this.name = name;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public static Place of(String name, String address, long latitude, long longitude) {
+        return Place.builder()
+                .name(name)
+                .address(address)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build();
+    }
+}
