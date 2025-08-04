73edc30b4df2b3493a4d820990b5b45bc404bdf3
hyoin <hyoinyang02@gmail.com>
2025-08-04 18:41:08 +0900

    [refactor] 디렉토리 분류

---
+++ src/main/java/com/wayble/server/direction/entity/transportation/Route.java
@@ -0,0 +1,36 @@
+package com.wayble.server.direction.entity.transportation;
+
+import com.wayble.server.direction.entity.DirectionType;
+import com.wayble.server.direction.entity.Node;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "route")
+public class Route {
+
+    @Id
+    private Long routeId;
+
+    @Column(name = "routeName", nullable = false)
+    private String routeName; //노선명(번호/호선)
+
+    @Column(name = "routeType", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private DirectionType routeType; // SUBWAY, BUS
+
+    // 기점 정류장
+    @ManyToOne
+    @JoinColumn(name = "start_node_id")
+    private Node startNode;
+
+    // 종점 정류장
+    @ManyToOne
+    @JoinColumn(name = "end_node_id")
+    private Node endNode;
+}
