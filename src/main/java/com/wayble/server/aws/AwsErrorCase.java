081392601848e279213f5a4d5c7445fad3241322
KiSeungMin <ksm011212@gmail.com>
2025-08-06 21:37:54 +0900

    [feat] 파일 업로드 관련 예외 객체 정의

--- src/main/java/com/wayble/server/aws/AwsErrorCase.java
+++ src/main/java/com/wayble/server/aws/AwsErrorCase.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum AwsErrorCase implements ErrorCase {
 
-    IMAGE_UPLOAD_ERROR(400, 8001, "파일 업로드 과정에서 문제가 발생했습니다.");
+    IMAGE_UPLOAD_ERROR(400, 8001, "이미지 업로드 과정에서 문제가 발생했습니다."),
+    IMAGE_DELETE_ERROR(400, 8002, "이미지 삭제 과정에서 문제가 발생했습니다.");
 
     private final Integer httpStatusCode;
     private final Integer errorCode;
