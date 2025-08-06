7ee87ef841e02e0b4cc4c2dd67af2081e9f58bb0
KiSeungMin <ksm011212@gmail.com>
2025-08-06 21:59:42 +0900

    [feat] 관리자 페이지에서 웨이블존 이미지 등록/수정 기능 구현

--- src/main/java/com/wayble/server/common/config/SecurityConfig.java
+++ src/main/java/com/wayble/server/common/config/SecurityConfig.java
@@ -51,7 +51,8 @@ public class SecurityConfig {
                                 "/",
                                 "/index",
                                 "/index.html",
-                                "/admin/**"
+                                "/admin/**",
+                                "/api/v1/files/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
