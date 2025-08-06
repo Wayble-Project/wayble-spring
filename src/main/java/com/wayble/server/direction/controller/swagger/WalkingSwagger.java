6a80a37b7860a58c1ffa7834656e0ac769089c57
zyovn <yui2507@naver.com>
2025-08-06 15:38:46 +0900

    [docs] walkingSwagger 수정

--- src/main/java/com/wayble/server/direction/controller/swagger/WalkingSwagger.java
+++ src/main/java/com/wayble/server/direction/controller/swagger/WalkingSwagger.java
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Tag(name = "[도보]", description = "도보 길찾기 관련 API")
+@Tag(name = "[길찾기 - 도보]", description = "도보 길찾기 관련 API")
 public interface WalkingSwagger {
     @Operation(
             summary = "도보 최적 경로 길찾기 API",
