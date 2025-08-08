46770856a66c93f4b23f78b3c546c842dab850ff
seungin <tmddls91@inha.edu>
2025-08-08 21:42:47 +0900

    [fix] 레포지토리에서 필요한 연관들을 미리 로딩하도록 수정

--- src/main/java/com/wayble/server/wayblezone/repository/WaybleZoneRepository.java
+++ src/main/java/com/wayble/server/wayblezone/repository/WaybleZoneRepository.java
@@ -1,12 +1,34 @@
 package com.wayble.server.wayblezone.repository;
 
 import com.wayble.server.wayblezone.entity.WaybleZone;
+import com.wayble.server.wayblezone.entity.WaybleZoneType;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface WaybleZoneRepository extends JpaRepository<WaybleZone, Long>, WaybleZoneRepositoryCustom {
-    List<WaybleZone> findByAddress_CityContainingAndZoneType(String city, com.wayble.server.wayblezone.entity.WaybleZoneType category);
+
+    // 목록 조회용: 시설 + 이미지 (운영시간 X)
+    @EntityGraph(attributePaths = {"facility", "waybleZoneImageList"})
+    @Query("""
+           select distinct z
+           from WaybleZone z
+           where z.address.city like concat('%', :city, '%')
+             and z.zoneType = :category
+           """)
+    List<WaybleZone> findSummaryByCityAndType(String city, WaybleZoneType category);
+
+    // 상세 조회용: 시설 + 이미지 + 운영시간
+    @EntityGraph(attributePaths = {"facility", "waybleZoneImageList", "operatingHours"})
+    @Query("""
+           select distinct z
+           from WaybleZone z
+           where z.id = :id
+           """)
+    Optional<WaybleZone> findDetailById(Long id);
 
     long count();
 }
