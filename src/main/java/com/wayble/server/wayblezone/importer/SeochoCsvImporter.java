6a514f8c8a3ef8e027922dff5557da4e7ec221d6
KiSeungMin <ksm011212@gmail.com>
2025-08-09 15:53:10 +0900

    [fix] csv importer 프로필을 develop으로 임시 설정

--- src/main/java/com/wayble/server/wayblezone/importer/SeochoCsvImporter.java
+++ src/main/java/com/wayble/server/wayblezone/importer/SeochoCsvImporter.java
@@ -29,7 +29,7 @@ import java.util.Map;
 import static com.wayble.server.wayblezone.importer.CsvSupport.*;
 
 @Slf4j
-@Profile("local")
+@Profile("develop")
 @Component
 @RequiredArgsConstructor
 public class SeochoCsvImporter implements CommandLineRunner {
