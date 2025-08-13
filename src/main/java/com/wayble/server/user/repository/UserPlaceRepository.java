b02143d98b8e01a9bda8b90fdf63f9f0d5125daf
seungin <tmddls91@inha.edu>
2025-08-14 02:07:50 +0900

    [refactor] 사용자별 리스트 조회 정렬 메서드 추가

--- src/main/java/com/wayble/server/user/repository/UserPlaceRepository.java
+++ src/main/java/com/wayble/server/user/repository/UserPlaceRepository.java
@@ -3,8 +3,10 @@ package com.wayble.server.user.repository;
 import com.wayble.server.user.entity.UserPlace;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserPlaceRepository extends JpaRepository<UserPlace, Long> {
     Optional<UserPlace> findByUser_IdAndTitle(Long userId, String title);
+    List<UserPlace> findAllByUser_IdOrderByCreatedAtDesc(Long userId); // 사용자 리스트 전체(최신순)
 }
