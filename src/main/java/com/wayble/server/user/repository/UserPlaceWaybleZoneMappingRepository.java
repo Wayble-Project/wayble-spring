a547bc6e7c5a7f18e5be24b71ee236500702eaf9
seungin <tmddls91@inha.edu>
2025-08-14 02:06:40 +0900

    [refactor] 웨이블존 매핑 레포지토리에 쿼리,메서드 추가

--- src/main/java/com/wayble/server/user/repository/UserPlaceWaybleZoneMappingRepository.java
+++ src/main/java/com/wayble/server/user/repository/UserPlaceWaybleZoneMappingRepository.java
@@ -1,8 +1,10 @@
 package com.wayble.server.user.repository;
 
 import com.wayble.server.user.entity.UserPlaceWaybleZoneMapping;
+import com.wayble.server.wayblezone.entity.WaybleZone;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,4 +13,16 @@ public interface UserPlaceWaybleZoneMappingRepository extends JpaRepository<User
 
     @EntityGraph(attributePaths = {"userPlace", "waybleZone"})
     List<UserPlaceWaybleZoneMapping> findAllByUserPlace_User_Id(Long userId);
+
+    // 리스트 내부 웨이블존만 바로 반환
+    @Query("""
+            select m.waybleZone
+            from UserPlaceWaybleZoneMapping m
+            where m.userPlace.id = :placeId
+            """)
+    List<WaybleZone> findZonesByPlaceId(Long placeId);
+
+    long countByUserPlace_Id(Long placeId);
+
+    void deleteByUserPlace_IdAndWaybleZone_Id(Long placeId, Long waybleZoneId);
 }
