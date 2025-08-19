8503d61dc04fe81cf3d2b935d9144dd076d26f3c
seungin <tmddls91@inha.edu>
2025-08-19 11:38:39 +0900

    [refactor] Auth errorCode 숫자 변경

--- src/main/java/com/wayble/server/auth/exception/AuthErrorCase.java
+++ src/main/java/com/wayble/server/auth/exception/AuthErrorCase.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum AuthErrorCase implements ErrorCase {
-    UNAUTHORIZED(401, 1001, "인증 정보가 없거나 userId를 추출할 수 없습니다.");
+    UNAUTHORIZED(401, 7001, "인증 정보가 없거나 userId를 추출할 수 없습니다.");
 
     private final Integer httpStatusCode;
     private final Integer errorCode;
