47b3255d3e302ef59e842ff45833302de4234573
seungin <tmddls91@inha.edu>
2025-08-19 11:08:57 +0900

    [feat] Auth (리졸버) 관련 에러케이스 추가

---
+++ src/main/java/com/wayble/server/auth/exception/AuthErrorCase.java
@@ -0,0 +1,15 @@
+package com.wayble.server.auth.exception;
+
+import com.wayble.server.common.exception.ErrorCase;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum AuthErrorCase implements ErrorCase {
+    UNAUTHORIZED(401, 1001, "인증 정보가 없거나 userId를 추출할 수 없습니다.");
+
+    private final Integer httpStatusCode;
+    private final Integer errorCode;
+    private final String message;
+}
