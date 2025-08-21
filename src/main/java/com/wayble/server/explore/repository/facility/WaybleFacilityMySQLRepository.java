3a4bec38097ad6511324abbba5c0295b16401361
KiSeungMin <ksm011212@gmail.com>
2025-08-21 21:58:14 +0900

    [feat] 웨이블 시설 정보 조회 기능을 MySQL로 구현

---
+++ src/main/java/com/wayble/server/explore/repository/facility/WaybleFacilityMySQLRepository.java
@@ -0,0 +1,7 @@
+package com.wayble.server.explore.repository.facility;
+
+import com.wayble.server.explore.entity.WaybleFacilityMySQL;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WaybleFacilityMySQLRepository extends JpaRepository<WaybleFacilityMySQL, Long> {
+}
